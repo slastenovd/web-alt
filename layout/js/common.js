963442f2b2bc5b14ada3613572e5640257fd9e98
slastenovd <slastenovd@mail.ru>
2016-09-19 18:13:25 +1000

    9

---
+++ layout/js/common.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+	// Костыль - выравниваем сетку, т.к. рекламные блоки имеют разную высоту
+	var autoHeightADBlocks = function(){
+		var item_height = $('.item__thumbnail:first').height();
+		var item_pading_bottom = $('.item:first').css("padding-bottom").replace("px", "");
+		var windowWidth = $(window).width() + 17;
+		var resHeight = 0;
+		if ( windowWidth > 599 ) {
+			resHeight = item_height*2 + item_pading_bottom*2 +1;
+		} else {
+			resHeight = item_height*4 + item_pading_bottom*6 +2;
+		}
+		// console.log(windowWidth+' '+resHeight);
+		$('.ad-1').height( resHeight );
+		$('.ad-2').height( resHeight );
+	}
+
+	$(window).on('resize', function(){
+		autoHeightADBlocks();
+	});
+
+	autoHeightADBlocks();
+
+});
